f682be478fb31003f8c3ba4aac380b4bbe24deec
Katrina Hirsch <katrina.hirsch@gmail.com>
2016-10-31 12:13:43 -0700

    change transitionTo target to an actual route

--- app/routes/question.js
+++ app/routes/question.js
@@ -19,7 +19,7 @@ export default Ember.Route.extend({
         }
       });
       question.save();
-      this.transitionTo('question-detail');
+      this.transitionTo('question');
     }
   }
 });
