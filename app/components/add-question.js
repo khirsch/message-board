f57ae9f52e3379eb66caf747533bdf9f2295ef8c
Katrina Hirsch <katrina.hirsch@gmail.com>
2016-11-04 12:40:16 -0700

    add helper to display timestamp on questions

--- app/components/add-question.js
+++ app/components/add-question.js
@@ -10,7 +10,8 @@ export default Ember.Component.extend({
       var params = {
         author: this.get('author') ? this.get('author') : "",
         content: this.get('content') ? this.get('content') : "",
-        notes: this.get('notes') ? this.get('notes') : ""
+        notes: this.get('notes') ? this.get('notes') : "",
+        postDate: moment().valueOf()
       };
       this.set('author', '');
       this.set('content', '');
