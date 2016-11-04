5434cf6a27070190d576a4d064bd8a0fe5a7396e
Katrina Hirsch <katrina.hirsch@gmail.com>
2016-11-04 10:55:23 -0700

    add computed property to count + display number of answers per question

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  answerCount: Ember.computed('question.answers', function () {
+    var answers = this.get('question.answers');
+    return answers.get('length');
+  }),
   actions: {
     saveAnswer(answer) {
       this.sendAction('saveAnswer', answer);
